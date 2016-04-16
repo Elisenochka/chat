c90fe8c881e3dab0892a26bba6c6c44c8c17e592
Elina <eli.lisenochka@gmail.com>
2016-04-16 18:52:53 +0300

    choose added

--- MainClass.java
+++ MainClass.java
@@ -1,6 +1,8 @@
 import Server.ClientHandler;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,23 +10,22 @@ import java.net.Socket;
  * Created by Romych on 13.04.2016.
  */
 public class MainClass {
-    public static void main(String[] args) {
 
 
-        try{
-            while(true){
-                Socket socket = server.accept();
-                new Thread(new ClientHandler(socket)).start();
-                System.out.println("Client connected");
-
-            }
-            System.out.println("Connection is open");
-
-            server.close();
-
-
-        } catch (IOException e){
-            e.printStackTrace();
+    public static void main(String[] args) throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Print how you would like to act s/c?");
+        if(reader.readLine()=="s"){
+            new ClientHandler(new ServerSocket(8189));
         }
+        else if(reader.readLine()=="c"){
+            System.out.println("What is your name?");
+            reader.readLine();
+        }
+
+
+
+
+
     }
 }
