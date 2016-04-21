3859410b62061afcb1f6f694f2d9fb1dadfe3068
Elina <eli.lisenochka@gmail.com>
2016-04-21 18:32:46 +0300

    keyListener added

--- Window.java
+++ Window.java
@@ -12,18 +12,17 @@ import java.io.BufferedWriter;
 /**
  * Created by Romych on 15.04.2016.
  */
-public class Window extends JFrame implements ActionListener{
-    public static JTextArea ta = new JTextArea();
+public class Window extends JFrame{
     int lines =0;
     public JLabel[] text = new JLabel[lines];
-    public Window(String name, BufferedWriter writer){
+
+    public Window(final String name, final String text){
         setTitle(name);
         setSize(400,400);
         setLocation(1200,200);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         final JTextArea jta = new JTextArea();
-        JButton jb1 = new JButton("Send");
-        jb1.setMnemonic(KeyEvent.VK_ENTER);
+        final JButton jb1 = new JButton("Send");
         jta.setFont(new Font("Times New Roman", Font.ITALIC,14));
         jta.setBackground(Color.lightGray);
         add(jta);
@@ -37,34 +36,40 @@ public class Window extends JFrame implements ActionListener{
         southPanel.setLayout(new BorderLayout());
         southPanel.add(jtf, BorderLayout.CENTER);
         southPanel.add(jb1, BorderLayout.EAST);
-        jb1.addActionListener(this);
+        //jb1.addActionListener();
         //jb1.setMnemonic(KeyEvent.KEY_PRESSED);
         //JRootPane rp = SwingUtilities.getRootPane(jb1);
         //rp.setDefaultButton(jb1);
         //
         jtf.addKeyListener(new KeyListener() {
-            public void actionPerformed(ActionEvent e) {
+            @Override
+            public void keyTyped(KeyEvent e){
+            }
+
+        public void keyPressed(KeyEvent e){
+            }
+
+            public void keyReleased(KeyEvent e){
                 if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    jta.append(jtf.getText() + '\n');
+                    jtf.setText("");
                 }
-                jb1.addActionListener(new
-
-                                              ActionListener() {
-                                                  @Override
-                                                  public void actionPerformed(ActionEvent e) {
-                                                      jta.append(jtf.getText() + '\n');
-                                                      //;
-                                                      jta.append(ta.getText());
-                                                      jtf.setText("");
-
-
-                                                  }
-                                              }
-
-                );
+            }
+        });
+                jb1.addActionListener(new ActionListener() {
+            @Override
+                 public void actionPerformed(ActionEvent e) {
+                       jta.append(text);
+                    }
+        });
 
                 setVisible(true);
 
 
-            }
+    }
 
-        }
+    public void printInWindow(String text){
+
+    }
+
+}
