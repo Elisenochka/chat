dc652bcc7e1906c0392cbf089beae0e1b4b74b0c
Elina <eli.lisenochka@gmail.com>
2016-04-21 18:34:20 +0300

    server

--- ClientHandler.java
+++ ClientHandler.java
@@ -1,9 +1,9 @@
-package Server;
-
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.*;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -14,33 +14,24 @@ public class ClientHandler extends Thread {
 
     Socket socket;
     BufferedReader reader;
-    PrintWriter writer;
+    BufferedWriter writer;
+    Window window;
     public static int clientsCount = 0;
     ServerSocket server;
 
-    public ClientHandler(ServerSocket serverSocket){
-        this.server = serverSocket;
-        try{
-            socket = server.accept();
-            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            writer = new PrintWriter(socket.getOutputStream());
-
-        }catch(IOException e){
-            e.printStackTrace();
-        }
-    }
-
     public void run(){
         try{
-            //reader = new BufferedReader(new InputStreamReader(server.getInputStream()));
-            //writer = new PrintWriter(server.getOutputStream());
-            writer.println("Hello, user");
+            server = new ServerSocket(8189);
+            socket = server.accept();
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             String str ="";
+            window=new Window("server",str);
             while(!str.equals("end")){
-                if(!reader.equals(null)){
+                //if(!reader.equals(null)){
                     str = reader.readLine();
-                    System.out.println(str);
-                }
+                    //System.out.println(str);
+                //}
             }
             socket.close();
         }catch(IOException e){
